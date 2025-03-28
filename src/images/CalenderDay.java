2ecef4cc1148dbe1b84ce407da2b4c568e0fc706
Nikki <nikkizhang320@gmail.com>
2025-03-28 13:58:27 -0400

    created a calender day class

---
+++ src/images/CalenderDay.java
@@ -0,0 +1,17 @@
+//package images;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//
+//public class CalenderDay {
+//    private String[] dialogue = {};
+//    private int dayNumber;
+//
+//    public CalenderDay(int dayNumber) {
+//        this.dayNumber = dayNumber;
+//    }cre
+//
+//    public static String getDialogue(int dayNumber) {
+//        return dialogue[]
+//    }
+//}
