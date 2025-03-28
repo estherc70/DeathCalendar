ced2a348e27ad41b6520c2da5aaf0020d9d984f2
Nikki <nikkizhang320@gmail.com>
2025-03-28 14:22:31 -0400

    created a panel class to abstract the panels

---
+++ src/PanelClass.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelClass extends JPanel {
+
+    private String fileName;
+
+    // Constructor to accept the image file name
+    public PanelClass(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); // Ensure the panel is properly rendered
+        // Load the background image
+        ImageIcon backgroundIcon = new ImageIcon(fileName);
+        Image backgroundImage = backgroundIcon.getImage();
+
+        // Scale the image to fit the size of the panel
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+    }
+
+    public static JPanel createPanel(String fileName) {
+        return new PanelClass(fileName);
+    }
+}
